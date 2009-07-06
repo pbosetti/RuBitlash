3c3bdd5d566d98ceb0850e637bc1e9da2f571a86
Paolo Bosetti <paolo.bosetti@me.com>
2009-07-06 16:32:29 +0200

    Added fading cycle to example/simple.rb

--- example/simple.rb
+++ example/simple.rb
@@ -14,4 +14,14 @@ bl.talk "d13=1"
 sleep 2
 bl[:f] = bl.free
 puts "f = #{bl[:f]}"
+
+puts "Starting cyclic fade"
+bl.pinmode(11,1)
+t = t0 = Time.now.to_f
+while (t - t0) < 5 do
+  t = Time.now.to_f
+  bl[:a11] = ((1.0 - Math::cos((t - t0)*Math::PI))*100).to_i # 2 Hz cycle
+  sleep 0.01
+end
+
 bl.close
